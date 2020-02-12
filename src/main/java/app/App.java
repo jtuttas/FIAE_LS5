0b2b15a4689fe51cd5d27f03f6690f4a9a0eb3f3
jtuttas <jtuttas@gmx.net>
2020-02-12 13:00:35 +0100

    tst

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,15 +1,16 @@
 package app;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.json.JSONObject;
 
 public class App {
     
+    /**
+     * Constructor
+     */
     public App() throws IOException {
         URL url = new URL("http://192.168.178.87:8080/get?pressure");
 
@@ -32,10 +33,19 @@ public class App {
         System.out.println("Druck ist:"+obj.getJSONObject("buffer").getJSONObject("pressure").getJSONArray("buffer").getDouble(0)+" mBAR");
     }
 
+    /**
+     * Max Funktion
+     * @param a
+     * @param b
+     * @return max of a , b
+     */
     public static int max(int a, int b) {
         return b;
     }
 
+    /**
+     * main method
+     */
     public static void main(String[] args) throws Exception {
         new App();
     }
