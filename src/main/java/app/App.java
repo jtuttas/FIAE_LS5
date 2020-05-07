8a3c32d9cc8094258e44d4021ce9c950b2c07c32
jtuttas <jtuttas@gmx.net>
2020-05-07 09:13:57 +0200

    nur noch main

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,20 +1,18 @@
 package app;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
-
 import org.json.JSONObject;
 
 public class App {
     
+
     /**
-     * Constructor
+     * main method
      */
-    public App() throws IOException {
+    public static void main(String[] args) throws Exception {
         URL url = new URL("http://192.168.178.87:8080/get?pressure");
 
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -26,26 +24,8 @@ public class App {
             inputLine=inputLine+reader.nextLine();
         }
         reader.close();
-        
+
         System.out.println("HTTP Responce: "+inputLine);
         JSONObject obj = new JSONObject(inputLine);
-        System.out.println("Druck ist:"+obj.getJSONObject("buffer").getJSONObject("pressure").getJSONArray("buffer").getDouble(0)+" mBAR");
-    }
-
-    /**
-     * Max Funktion
-     * @param a
-     * @param b
-     * @return max of a , b
-     */
-    public static int max(int a, int b) {
-        return b;
-    }
-
-    /**
-     * main method
-     */
-    public static void main(String[] args) throws Exception {
-        new App();
-    }
+        System.out.println("Druck ist:"+obj.getJSONObject("buffer").getJSONObject("pressure").getJSONArray("buffer").getDouble(0)+" mBAR");    }
 }
--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -9,8 +9,4 @@ import org.junit.Test;
 public class AppTest {
 
 
-    @Test
-    public void maxTest() {
-       Assert.assertEquals("Max von 5 und 6 sollte 6 sein!", 6, App.max(5, 6));
-    }
 }
