45f494172586724cd85ef0fd49ea1eef32afe89f
jtuttas <jtuttas@gmx.net>
2020-05-07 09:03:20 +0200

    schöner mit Scanner

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,12 @@
 package app;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
+
 import org.json.JSONObject;
 
 public class App {
@@ -15,21 +18,17 @@ public class App {
         URL url = new URL("http://192.168.178.87:8080/get?pressure");
 
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-        int responseCode = con.getResponseCode();
         System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        Scanner reader = new Scanner(con.getInputStream());
+        String inputLine="";
+        while (reader.hasNext()) {
+            inputLine=inputLine+reader.nextLine();
         }
-        in.close();
-        System.out.println("HTTP Responce: "+response.toString());
-        JSONObject obj = new JSONObject(response.toString());
+        reader.close();
+        
+        System.out.println("HTTP Responce: "+inputLine);
+        JSONObject obj = new JSONObject(inputLine);
         System.out.println("Druck ist:"+obj.getJSONObject("buffer").getJSONObject("pressure").getJSONArray("buffer").getDouble(0)+" mBAR");
     }
 
